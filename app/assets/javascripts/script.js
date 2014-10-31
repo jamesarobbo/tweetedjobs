873fb06e42ff7c3cdbdf27f448dbaa5254e4a9cf
James Robinson <jamesarobinson6@gmail.com>
2014-10-31 15:31:36 +0000

    remove isFitWidth from masonry script

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
  $(function(){
    var $container = $('#container');
    $container.imagesLoaded( function () {
-       itemSelector: '.item',
-       isFitWidth: true
+       itemSelector: '.item'
+      
    });
   });
 

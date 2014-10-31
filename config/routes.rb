e52c186b91bdb4b5a3abdf16e019006f58280a0b
James Robinson <jamesarobinson6@gmail.com>
2014-10-31 17:14:23 +0000

    add no results logic

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ resources :tweets, only: [:index]
 
 root "tweets#index"
 
-get "/too_many_requests", to: "tweets#too_many_requests", as: :too_many_requests
+get "/requestlimit", to: "tweets#too_many_requests", as: :too_many_requests
 
 get "*path", to: redirect('/')
 
